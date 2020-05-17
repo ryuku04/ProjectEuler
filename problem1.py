9680f708ec9ae79e01876b6a00f92c0de4bf822a
ryuku04 <uminotairiku@gmail.com>
2020-05-17 17:27:08 +0900

    add command.txt

--- problem1.py
+++ problem1.py
@@ -5,7 +5,7 @@
 #*              that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
 #*              The sum of these multiples is 23.
 #*              Find the sum of all the multiples of 3 or 5 below 1000.
-#* lastupdate : 2019/05/06
+#* lastupdate : 2020/05/15
 #*******************************************************************************
 
 def get_sum_3or5mod(n):
